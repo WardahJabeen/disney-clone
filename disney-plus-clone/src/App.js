08716e1a29b01532692df7dc1ddce02c8ac91ddb
Wardah Jabeen <jabeen@usc.edu>
2024-01-24 12:38:11 -0800

    login page frontend done

--- disney-plus-clone/src/App.js
+++ disney-plus-clone/src/App.js
@@ -4,14 +4,17 @@ import Header from "./components/Header"
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/detail" element={<Detail />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
